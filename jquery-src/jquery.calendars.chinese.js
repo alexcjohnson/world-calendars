963a10ff888a4365a56ecce6bd118f049ebd6f29
Nicolas Riesco <enquiries@nicolasriesco.net>
2016-12-06 23:30:07 +0000

    chinese: format month numbers and names
    * Now `chineseCalendar.formatDate()` outputs Chinese months instead of
      month indices.

--- jquery-src/jquery.calendars.chinese.js
+++ jquery-src/jquery.calendars.chinese.js
@@ -59,9 +59,61 @@
             '': {
                 name: 'Chinese',
                 epochs: ['BEC', 'EC'],
-                monthNames: ['January', 'February', 'March', 'April', 'May', 'June',
-                'July', 'August', 'September', 'October', 'November', 'December'],
-                monthNamesShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+                monthNumbers: function(date, padded) {
+                    var year = ChineseCalendar.prototype._validateYear(date);
+                    var monthIndex = date.month();
+
+                    var month = '' +
+                        ChineseCalendar.prototype.toChineseMonth(year, monthIndex);
+
+                    var isIntercalary =
+                        ChineseCalendar.prototype.intercalaryMonth(year, monthIndex);
+                    if (isIntercalary) {
+                        month += 'i';
+                    }
+
+                    if (padded && month.length < 2) {
+                        month = "0" + month;
+                    }
+
+                    return month;
+                },
+                monthNames: function(date) {
+                    var year = ChineseCalendar.prototype._validateYear(date);
+                    var monthIndex = date.month();
+
+                    var month =
+                        ChineseCalendar.prototype.toChineseMonth(year, monthIndex);
+
+                    var monthName = ['一月','二月','三月','四月','五月','六月',
+                        '七月','八月','九月','十月','十一月','十二月'][month - 1];
+
+                    var isIntercalary =
+                        ChineseCalendar.prototype.intercalaryMonth(year, monthIndex);
+                    if (isIntercalary) {
+                        monthName = '闰' + monthName;
+                    }
+
+                    return monthName;
+                },
+                monthNamesShort: function(date) {
+                    var year = ChineseCalendar.prototype._validateYear(date);
+                    var monthIndex = date.month();
+
+                    var month =
+                        ChineseCalendar.prototype.toChineseMonth(year, monthIndex);
+
+                    var monthName = ['一','二','三','四','五','六',
+                        '七','八','九','十','十一','十二'][month - 1];
+
+                    var isIntercalary =
+                        ChineseCalendar.prototype.intercalaryMonth(year, monthIndex);
+                    if (isIntercalary) {
+                        monthName = '闰' + monthName;
+                    }
+
+                    return monthName;
+                },
                 dayNames: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
                 dayNamesShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
                 dayNamesMin: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
