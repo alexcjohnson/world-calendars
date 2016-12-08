2d7964faaa1228610b4785da96b481a643e6a8e4
alexcjohnson <alex@plot.ly>
2016-12-08 15:04:06 -0500

    build empties dist before running

--- bin/build.js
+++ bin/build.js
@@ -201,6 +201,25 @@ function srcInclude(fn) {
     return true;
 }
 
+function rmDir(dirPath, removeSelf) {
+    if (removeSelf === undefined) removeSelf = true;
+
+    try { var files = fs.readdirSync(dirPath); }
+    catch(e) { return; }
+
+    if (files.length > 0) {
+        for (var i = 0; i < files.length; i++) {
+            var filePath = dirPath + '/' + files[i];
+            if (fs.statSync(filePath).isFile()) fs.unlinkSync(filePath);
+            else rmDir(filePath);
+        }
+    }
+
+    if (removeSelf) fs.rmdirSync(dirPath);
+}
+
+rmDir(distDir, false);
+
 subdirectories.forEach(function(subdir) {
     fs.mkdirSync(distDir + subdir);
 });
